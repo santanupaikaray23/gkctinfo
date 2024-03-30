d9ca48953f7d066839047652ae4c1deb81f32e70
LAPTOP-KLF3DVEL\santa <santanupaikaray1996@gmail.com>
2024-03-30 07:32:00 +0530

    add all files and folders

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from './components/pages/HomePage/Home';
 import Footer from './components/pages/Footer/Footer';
 import ListingApi from './components/pages/listing/listingApi';
 import Detail from './components/pages/details/details';
+import PlaceBooking from './components/pages/booking/placeBooking';
+
 
 function App() {
   return (
@@ -14,6 +16,9 @@ function App() {
     <Route path='/' exact component={Home}/>
     <Route path="/list/:id" component={ListingApi}/>
     <Route path='/details/:id' component={Detail}/>
+    <Route path="/booking/:check_name" component={PlaceBooking}/>
+   
+        
    
    </Switch>
    <Footer/>
