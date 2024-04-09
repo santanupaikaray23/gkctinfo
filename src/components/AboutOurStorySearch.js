268fd863273bd418681b2568ad605ac9a614e699
LAPTOP-KLF3DVEL\santa <santanupaikaray1996@gmail.com>
2024-04-09 23:28:33 +0530

    add all changes

---
+++ src/components/AboutOurStorySearch.js
@@ -0,0 +1,51 @@
+import React, {Component} from 'react';
+import './AboutOurStorySearch.css';
+
+class AboutOurStorySearch extends Component{
+    render(){
+        return(
+            
+       
+              
+                <div className='about' id='about'>
+                <div className='left'>
+                    <div className='wrapper1'>
+               <h2>The Story Of Gkct Info</h2>
+        <p>
+        Here are some real life success stories of starting a
+         digital marketing business: Digital Marketing Business 
+         Success Stories.
+        </p>
+        </div>
+                   </div>
+                <div className='right'>
+                <div className='AboutContainer'>
+                    <div className='class="elementor-widget-container"'>
+                    <div className='video-popup-wrapper'>
+                    <a className='swipebox-video video-popup_link' href="https://www.youtube.com/watch?v=7K43sEjKWiE" data-settings="{autoplay:false}">
+                    <svg class="play-icon" viewBox="0 0 13 18" xmlns="http://www.w3.org/2000/svg">
+        <polygon points="1,1 1,17 12,9" fill="currentColor" stroke="black" stroke-width="1"/>
+    </svg>
+                    </a>
+                    </div>
+                   </div>
+                 
+                    
+                   
+                    </div>
+                   
+                   
+    
+                    
+    
+            </div>
+            
+        </div>
+        
+    
+            )
+            
+      
+    }
+}
+export default AboutOurStorySearch;
--- src/components/pages/About/About.js
+++ src/components/pages/About/About.js
@@ -3,6 +3,7 @@ import AboutSearch from '../../AboutSearch'
 import AboutQuickSearch from '../../AboutQuickSearch'
 // import {homeObjOne} from './Data'
 import AboutOurStory from '../../AboutOurStory'
+import AboutOurStorySearch from '../../AboutOurStorySearch'
 
 
 function About() {
@@ -11,6 +12,7 @@ function About() {
       <AboutSearch/>
       <AboutQuickSearch/>
       <AboutOurStory/>
+      <AboutOurStorySearch/>
       
     </>
   )
