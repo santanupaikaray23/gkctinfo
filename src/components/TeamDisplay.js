02e69729cc3a302f9ba37395419d6b067e2ae04f
LAPTOP-KLF3DVEL\santa <santanupaikaray1996@gmail.com>
2024-04-15 15:14:16 +0530

    add al chnages

---
+++ src/components/TeamDisplay.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './TeamQuickSearch.css';
+import {Link} from 'react-router-dom';
+
+const TeamDisplay = (props)=>{
+   const listService = ({teamdata})=>{
+    if(teamdata){
+        return teamdata.map((item)=>{
+            return(
+                <Link to={`/list/${item._id}`}>
+                    <div className='tileContainer'>
+                        <div className='tileComponent1'>
+                            <img src={item.image} alt=""/>
+
+                        </div>
+                        <div className='tileComponent2'>
+                            <div className='componentHeading'>
+                                {item.name}
+                             </div>
+                             <div className='componentSubHeading'>
+                                {item.designation}
+
+                             </div>
+
+                        </div>
+                    </div>
+                    </Link>
+            )
+        })
+    }
+
+   }
+   return(
+    <div className='quickSearchContainer'>
+        <p6>OUR TEAM</p6>
+        <p className='quickSearchHeading'>
+        Our Professionals
+
+        </p>
+        <p className='quickSearchSubHeading3'>
+        Our professional helps a brand connect with potential customers over the 
+        internet and increase its reach for a better business opportunity 
+
+        </p>
+        <br/>
+        {listService(props)}
+    </div>
+   
+   )
+}
+export default TeamDisplay;
--- src/components/TeamQuickSearch.js
+++ src/components/TeamQuickSearch.js
@@ -1,13 +1,31 @@
 import React, {Component} from 'react';
-import './TeamSearch.css';
+import TeamDisplay from '../components/TeamDisplay';
+
+
+ const url = "https://node-api-freelance.onrender.com/teams";
 
 class TeamQuickSearch extends Component{
+    constructor(){
+        super()
+
+        this.state={
+            teamType:''
+        }
+    }
     render(){
         return(
-            
-            <div>Work in Progress</div>
-            
+           <TeamDisplay teamdata={this.state.teamType}/>
         )
     }
+
+    componentDidMount(){
+        fetch(url,{method:"GET"})
+        .then((res)=>res.json())
+        .then((data)=>{
+            this.setState({teamType:data})
+        })
+    }
+
+  
 }
 export default TeamQuickSearch;
